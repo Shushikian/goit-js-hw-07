39139a3e03f6b9d661d798fb592e21c30d8a2ff5
Dmitry Shushaiev <shushikian@gmail.com>
2022-06-16 21:46:39 +0300

    DZ 1 - done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imageList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const imageListItem = galleryItems.map(({ preview, original, description }) =>
+
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      
+    />
+  </a>
+</div>`
+).join('');
+
+
+imageList.insertAdjacentHTML(`afterbegin`, imageListItem);
+// // console.log(galleryItems);
+
+imageList.addEventListener('click', showOrigin);
+
+function showOrigin(event) {
+  
+  event.preventDefault();
+
+  const modalWindow = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`);
+  modalWindow.show();
+};
